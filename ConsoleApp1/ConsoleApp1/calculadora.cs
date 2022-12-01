51e2b828da65c112aefe413245286ec34cb4b6d0
Denilson Oliveira <89954041+denilsude@users.noreply.github.com>
2022-11-30 21:08:38 -0300

    posso modificar as cores

---
+++ ConsoleApp1/ConsoleApp1/calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("insira o primeiro valor : ");
+            string valor1 = Console.ReadLine(); 
+            Console.WriteLine();
+
+            Console.WriteLine("insira o segundo valor: ");
+            string valor2 = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("insira um Operador: (+ - * / ): ");
+            string operador = Console.ReadLine();
+            Console.WriteLine();
+
+            decimal Resultado = 0;
+            if (operador == "+")   
+            Resultado = Convert.ToInt16(valor1)  + Convert.ToInt16(valor2);
+            if (operador == "-")
+                Resultado = Convert.ToInt16(valor1) - Convert.ToInt16(valor2);
+            if (operador == "*")
+                Resultado = Convert.ToInt16(valor1) * Convert.ToInt16(valor2);
+            if (operador == "/")
+                Resultado = Convert.ToInt16(valor1) / Convert.ToInt16(valor2);
+
+            Console.WriteLine("O resultado é: " + Resultado);
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
